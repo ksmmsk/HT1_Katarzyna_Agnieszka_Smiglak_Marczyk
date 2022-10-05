69b22433044f975b92e9428772bc06b29146efdf
katsm <k.smiglak.marczyk@gmail.com>
2022-10-05 21:00:30 +0200

    make the constants names uniform

--- test/constants/job.data.js
+++ test/constants/job.data.js
@@ -1,7 +1,7 @@
 class JobData {
     static TITLE1 = 'Intern';
     static TITLE2 = 'Student';
-    static DESC = 'A description';
+    static DESC1 = 'A description';
     static DESC2 = 'A new description';
     static NOTE = 'A job title note';
 }
